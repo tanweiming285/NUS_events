c4705996edbc795000d63c7473f9cb9d49eea650
tanweiming285 <tanweiming285@gmail.com>
2015-08-13 12:50:25 +0800

    Added app launcher logo, added internet permission, added logo in login page, changed background color, coded onclick method.

--- app/src/main/java/mingbin/nusevents/navigationActivity.java
+++ app/src/main/java/mingbin/nusevents/navigationActivity.java
@@ -1,28 +1,22 @@
 package mingbin.nusevents;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class navigationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_navigation);
     }
-    public void login(View view){
-        Intent intent = new Intent(this,navigationActivity.class);
-        startActivity(intent);
-        finish();
-    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_navigation, menu);
         return true;
     }
 
